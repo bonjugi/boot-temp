567851c1a56fe8d176e157ce1bbbfcc12ed0e35a
bonjugi <vjava114@gmail.com>
2019-03-20 17:58:49 +0900

    11장까지 완료! 이제 12장 (드디어 더하기) 를 하자!

--- src/main/java/dev/bonjugi/boottemp/tdd/Money.java
+++ src/main/java/dev/bonjugi/boottemp/tdd/Money.java
@@ -1,23 +1,49 @@
 package dev.bonjugi.boottemp.tdd;
 
-public abstract class Money {
+public class Money {
 
-	public static Franc franc(int multiplier) {
-		return new Franc(multiplier);
-
-	}
-
-	abstract Money times(int multiplier);
-
+	/**
+	 * member variables
+	 */
 	protected int amount;
 
-	public static Dollar dollar(int amount) {
-		return new Dollar(amount);
+	protected String currency;
+
+
+	public Money times(int multiplier) {
+		return new Money(amount * multiplier, currency);
+	}
+
+
+
+	/**
+	 * factory method
+	 */
+	public static Money franc(int multiplier) {
+		return new Money(multiplier,"CHF");
+	}
+	public static Money dollar(int amount) {
+		return new Money(amount,"USD");
+	}
+
+
+	/**
+	 * constructor
+	 */
+	public Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
+
+
+
+	public String currency() {
+		return this.currency;
 	}
 
 
 	public boolean equals(Object obj) {
 		Money money = (Money) obj;
-		return amount == money.amount && getClass().equals(money.getClass());
+		return amount == money.amount && currency.equals(money.currency());
 	}
 }
--- src/test/java/dev/bonjugi/boottemp/tdd/TempTest.java
+++ src/test/java/dev/bonjugi/boottemp/tdd/TempTest.java
@@ -10,27 +10,24 @@ public class TempTest {
 
 
 	@Test
+	public void testCurrency(){
+		assertEquals("USD", Money.dollar(1).currency());
+		assertEquals("CHF", Money.franc(1).currency());
+	}
+
+
+	@Test
 	public void testMultiplication(){
 		Money five = Money.dollar(5);
 		assertThat(Money.dollar(10), is(five.times(2)));
 		assertThat(Money.dollar(15), is(five.times(3)));
 
 	}
-	@Test
-	public void testFrancMultiplication(){
-		Franc five = Money.franc(5);
-		assertThat(Money.franc(10), is(five.times(2)));
-		assertThat(Money.franc(15), is(five.times(3)));
-	}
 
 	@Test
 	public void tetEquality(){
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
-
-
 		assertThat(Money.franc(5), not(Money.dollar(5)));
 	}
 
