6dab08fe4380c66fcb50416158c51eb429b644fd
bonjugi <vjava114@gmail.com>
2019-04-20 20:06:56 +0900

    11장. 모든 악의 근원 - 하위클래스 제거. 및 불필요 테스트 제거

--- src/test/java/dev/bonjugi/boottemp/tddbe2/TddTest.java
+++ src/test/java/dev/bonjugi/boottemp/tddbe2/TddTest.java
@@ -26,8 +26,6 @@ public class TddTest {
     public void testEquality(){
         assertTrue(Money.dollar(5).equals(Money.dollar(5)));
         assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
         assertFalse(Money.franc(5).equals(Money.dollar(6)));
     }
 
@@ -36,11 +34,4 @@ public class TddTest {
     	assertEquals("USD", Money.dollar(1).currency());
     	assertEquals("CHF", Money.franc(1).currency());
     }
-
-
-    @Test
-	public void testDifferentClassEquality(){
-	    assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")) );
-    }
-
 }
